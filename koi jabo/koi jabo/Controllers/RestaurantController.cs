108298806de9f3f7f8197e0da90caf2aef03af62
tareq <tareqaziz.aust.cse@gmail.com>
2015-12-29 09:30:50 +0600

    mongo conn string changed to koijabo db and getall controller added

--- koi jabo/koi jabo/Controllers/RestaurantController.cs
+++ koi jabo/koi jabo/Controllers/RestaurantController.cs
@@ -1,6 +1,8 @@
 ﻿using koi_jabo.Entity;
 using koi_jabo.Lib.DBConnection;
 using koi_jabo.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,17 @@ namespace koi_jabo.Controllers
 
             await context.Restaurants.InsertOneAsync(new RestaurantEntity(model));
             return Json(model);
+        }
+
+        //public async Task<IEnumerable<TrackerInfo>> Find(bool isAssigned, int skip, int limit)
+        //{
+        //    return await this._context.Trackers.Find(x => x.IsAssigned == isAssigned).Skip(skip).Limit(limit).ToListAsync();
+        //}
+        public async Task<IHttpActionResult> Getall()
+        {
+            var filter = new BsonDocument();
+            var all = await context.Restaurants.Find(filter: filter).ToListAsync();
+            return Json(all);
         }
     }
 }
